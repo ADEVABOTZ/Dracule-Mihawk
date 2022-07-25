825f3f9dd4d71e4c67f81aadd63dc9ca42f27b9d
ADEVABOTZ <106531830+ADEVABOTZ@users.noreply.github.com>
2022-07-25 22:06:48 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -51,7 +51,7 @@ prem: '*Khusus User Premium*'
 
 //===> KALAU GANTI FOTO GANTI JUGA NAMA NYA
 global.thumb = fs.readFileSync('./media/Tak berjudul1_20220614104451.png')
-global.donasi = fs.readFileSync('./media/donasi.jpg')
+global.donasi = fs.readFileSync('./media/IMG-20220613-WA0020.jpg')
 global.thumbnail = fs.readFileSync('./media/Tak berjudul1_20220614104451.png')
 
 //===> UCAPAN WELCOME AND LEAVE
